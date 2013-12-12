781b842d09b365c951605fc2a119f70bd95c6e64
Shuo Zhang <shuo.zhang.us@gmail.com>
2013-12-12 12:44:09 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meerkats::Application.config.secret_key_base = '7a8506141d3ef4d7ab8f1920ece85e62a17bd2b4c2c001c0ab37c1e19ba8665cc0902c0c0819080e7620014faeea1932948ce1f643d14377390a807ef0ab78d9'
